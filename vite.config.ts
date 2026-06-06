98bdf4ab8407bce721afa5141d0e200404a23576
Althukuriya <althukuriya83@gmail.com>
2026-06-06 11:49:19 +0530

    Fix Netlify base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cart/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
